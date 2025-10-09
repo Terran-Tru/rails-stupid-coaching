cb0157050e9d8020636c0d95305c178f5f99cbb8
Oscar Lara <oscar.lara.a@gmail.com>
2025-10-09 17:55:43 -0500

    stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,16 @@ class QuestionsController < ApplicationController
   def ask
     
   end
-
+  
   def answer
-    
+    @member = params[:member].capitalize
+
+    if @member == "I am going to work"
+      @placeholder = "Great!"
+    elsif @member&.end_with?("?")
+      @placeholder = "Silly question, get dressed and go to work!"
+    else
+      @placeholder = "I don't care, get dressed and go to work!"
+    end
   end
 end
